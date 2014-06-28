9ad211e35307d599205e18baf37380e706053564
Jad <jadinvt@gmail.com>
2014-06-27 21:39:37 -0500

    Added parameter to dropcopy for 'non-standard' directory.

--- scripts/drop_copy.py
+++ scripts/drop_copy.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 import os
 import re
+import sys
 
-dropbox_dir = '/home/jadavis/Dropbox/Camera Uploads'
-target_dir = '/home/jadavis/Desktop/latest_uploads'
+#dropbox_dir = '/home/jadavis/Dropbox/Camera Uploads'
+#target_dir = '/home/jadavis/Desktop/latest_uploads'
+dropbox_dir = '/mnt/media/Pictures/2014/06/09'
+target_dir = '/mnt/media/Pictures/2014/06/09'
 
 # fullpath = os.path.abspath(target)
 #(year, month, day) = fullpath.split("/")[4:7]
@@ -11,7 +14,9 @@ target_dir = '/home/jadavis/Desktop/latest_uploads'
 #path = "/mnt/media/honora_pics/scaled/%s/%s/%s/" % (year, month, day)
 
 
-def main():
+def main(single_dir=None):
+    if single_dir:
+        dropbox_dir = target_dir = single_dir
     for file in os.listdir(dropbox_dir):
         match = re.search(
             '(\d{4})-(\d{2})-(\d{2}) (\d{2})\.(\d{2}).(\d{2}).(\w+)', file)
@@ -24,4 +29,6 @@ def main():
             result = os.rename(old_file, new_file)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) == 2:
+        single_dir = sys.argv[1]
+    main(single_dir)
